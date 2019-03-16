72f73d779339caa061ca1f47d934b12fbde3d417
edsilv <e.silverton@gmail.com>
2019-03-16 14:26:10 +0000

    omit content-type header

--- src/components/AnnoStoreQuery.js
+++ src/components/AnnoStoreQuery.js
@@ -172,9 +172,9 @@ export default class AnnoStoreQuery extends Component {
       method: "GET", // *GET, POST, PUT, DELETE, etc.
       mode: "cors", // no-cors, cors, *same-origin
       cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: "same-origin", // include, *same-origin, omit
+      credentials: "omit", // include, *same-origin, omit
       headers: {
-        "Content-Type": "application/json"
+        //"Content-Type": "application/json"
       },
       redirect: "follow", // manual, *follow, error
       referrer: "no-referrer" // no-referrer, *client
@@ -188,9 +188,9 @@ export default class AnnoStoreQuery extends Component {
       method: "POST", // *GET, POST, PUT, DELETE, etc.
       mode: "cors", // no-cors, cors, *same-origin
       cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: "same-origin", // include, *same-origin, omit
+      credentials: "omit", // include, *same-origin, omit
       headers: {
-        "Content-Type": "application/json",
+        //"Content-Type": "application/json",
         // "Content-Type": "application/x-www-form-urlencoded",
       },
       redirect: "follow", // manual, *follow, error
